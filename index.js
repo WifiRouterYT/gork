f7b77de4d2af4609bf06ee69c176b7684c2da2d2
WifiRouterYT <ivan@zavyalov.com>
2025-10-19 01:50:50 -0700

    lil rework + HE LYIN!!

--- index.js
+++ index.js
@@ -6,25 +6,35 @@ const client = new Client({
   partials: [Partials.Channel]
 });
 
+const responses = [
+  "ON FOENEM!!!!!",
+  "HELL NAW!!!!!",
+  "HE LYIN!!"
+];
+
 client.on('messageCreate', message => {
     if (message.author.bot) return;
 
     if (message.mentions.users.has(client.user.id)) {
-      const botId = client.user.id;
+      /*const botId = client.user.id;
       const mentionRegex = new RegExp(`<@!?${botId}>`, 'g');
-      const cleaned = message.content.replace(mentionRegex, '').trim();
-      if(cleaned.includes("is this")) {
-        if(parseInt(Math.random() * 2) ?  true : false) {
-          message.reply("ON FOENEM!!!!!");
-        } else {
-          message.reply("HELL NAW!!!!!");
-        }
-      }                                                                                                                                                                                                                                                                                                                                                    /* not malware lmfao */ else if(cleaned.includes(Buffer.from("d2hvIG1hZGUgeW91", 'base64').toString('utf-8'))) {message.reply(Buffer.from("d2lmaXJvdXRydGVyIG1hZGUgbWUgOjM", 'base64').toString('utf-8'));}
+      const cleaned = message.content.replace(mentionRegex, '').trim(); 
+
+      if(cleaned.includes("is this")) { 
+      }*/
+
+      const response = responses[Math.floor(Math.random() * responses.length)];
+      const username = message.author.username;
+      const channel = message.channel.name;
+      const server = message.guild.name;
+
+      message.reply(response);
+      console.log(`grork >> Responded to @${username} in #${channel} (${server}) with "${response}"`);
     }
 });
 
 client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`\ngrork >> Locked and loaded as ${readyClient.user.tag}!`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
